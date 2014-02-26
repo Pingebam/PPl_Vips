8fb6d14e26a9bac291553c9667288bc8ad80559c
Pingebam <kontakt@pingebam.de>
2014-02-26 19:09:56 +0100

    +Konfiguration initialisiert
    +config.yml
    +plugin.yml

---
+++ src/de/pingebam/bukkit/ppl_vips/PPl_Vips.java
@@ -0,0 +1,17 @@
+package de.pingebam.bukkit.ppl_vips;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PPl_Vips extends JavaPlugin {
+	
+	public void onEnable(){
+		// TODO work
+		this.getServer().getLogger().info("PPl_Vips enabling finished.");
+	}
+	
+	public void onDisable(){
+		// TODO work
+		this.getServer().getLogger().info("PPl_Vips disabling finished.");
+	}
+	
+}
